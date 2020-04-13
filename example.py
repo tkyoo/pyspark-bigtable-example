9a0dc277f90963e97f63dfb3ba5ecd3e7fbe9035
Taekyung Yoo <tkyoo0627@gmail.com>
2020-04-13 21:59:39 +0900

    Update example.py

--- example.py
+++ example.py
@@ -5,8 +5,14 @@ from pyspark.sql import SparkSession
 dependencies = [
     "com.google.cloud.bigtable:bigtable-hbase-2.x-hadoop:1.14.0",
     "org.apache.hbase.connectors.spark:hbase-spark:1.0.0",
-    "org.apache.hbase:hbase-server:2.2.3",
+    "org.apache.hbase:hbase-server:2.2.4",
 ]
+
+# In this scenario, current path must be added to extraClassPath for finding hbase-site.xml in HBaseContext object.
+# credentials.json: GCP service account json key that have proper authorization.
+# this setting (using current path credentials.json) is set on hbase-site.xml. 
+# you can find other options about GCP, Bigtable in https://github.com/googleapis/java-bigtable-hbase/blob/master/bigtable-client-core-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/BigtableOptionsFactory.java
+# BigtableOptionFactory.java
 
 spark = SparkSession.builder \
     .config("spark.driver.extraClassPath", "./") \
@@ -15,6 +21,7 @@ spark = SparkSession.builder \
     .config("spark.files", "hbase-site.xml,credentials.json") \
     .getOrCreate()
 
+# Catalog dictionary reference: https://hbase.apache.org/2.2/book.html#_sparksql_dataframes
 catalog = {
     "table": {
         "namespace": "default",
@@ -35,6 +42,7 @@ catalog = {
     }
 }
 
+# example data & catalog reference: https://github.com/GoogleCloudPlatform/cloud-bigtable-examples/tree/master/scala/bigtable-shc
 people = [
     { "name": "bill", "age": 30 },
     { "name": "solomon", "age": 27 },
@@ -45,6 +53,16 @@ people = [
     { "name": "jacek", "age": 45 },
 ]
 
+# You can find options like 'catalog', 'newtable' in https://github.com/apache/hbase-connectors/blob/master/spark/hbase-spark/src/main/scala/org/apache/hadoop/hbase/spark/datasources/HBaseTableCatalog.scala
+# HBaseTableCatalog object.
+# options like 'hbase.spark.use.hbasecontext', 'hbase.spark.query.timestamp' are in https://github.com/apache/hbase-connectors/blob/master/spark/hbase-spark/src/main/scala/org/apache/hadoop/hbase/spark/datasources/HBaseSparkConf.scala
+# HBaseSparkConf object.
+
+# The difference between 'hortonworks-spark/shc' and 'apache/hbase-connector' is a 'format'. 
+# If you use 'hortonworks-spark/shc', you must use 'org.apache.spark.sql.execution.datasources.hbase'.
+# In this scenario, I use 'apache/hbase-connector', thus this project must use 'org.apache.hadoop.hbase.spark' format. 
+
+# In my case, I must set 'hbase.spark.use.hbasecontext' to false. if not, hbase-connector cannot find 'hbase-site.xml' and you can see zookeepr connection errors.
 spark.createDataFrame(people).write \
     .option("catalog", json.dumps(catalog)) \
     .option("newtable", 5) \
